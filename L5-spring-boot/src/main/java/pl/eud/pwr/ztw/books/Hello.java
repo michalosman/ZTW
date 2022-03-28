7fd410e9c13dbb4c4bcc7d44d855358e365f96d1
michalosman <michal.osman.997@gmail.com>
2022-03-28 23:26:58 +0200

    Finish CRUD for Books

--- L5-spring-boot/src/main/java/pl/eud/pwr/ztw/books/Hello.java
+++ L5-spring-boot/src/main/java/pl/eud/pwr/ztw/books/Hello.java
@@ -2,6 +2,7 @@ package pl.eud.pwr.ztw.books;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class Hello {
     @RequestMapping("/")
--- L5-spring-boot/src/main/java/pl/eud/pwr/ztw/books/IBooksService.java
+++ L5-spring-boot/src/main/java/pl/eud/pwr/ztw/books/IBooksService.java
@@ -1,7 +1,15 @@
 package pl.eud.pwr.ztw.books;
 
 import java.util.Collection;
+
 public interface IBooksService {
     public abstract Collection<Book> getBooks();
+
     public abstract Book getBook(int id);
+
+    public abstract Book addBook(Book newBook);
+
+    public abstract Book updateBook(Book book);
+
+    public abstract boolean deleteBook(int id);
 }
