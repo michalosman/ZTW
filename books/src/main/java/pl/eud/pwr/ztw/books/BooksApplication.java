668679e2845e6322ae60a4c174b8d6e8cdda8cce
travelmate <a@a.a>
2022-03-28 20:02:28 +0200

    make from instruction

---
+++ books/src/main/java/pl/eud/pwr/ztw/books/BooksApplication.java
@@ -0,0 +1,16 @@
+package pl.eud.pwr.ztw.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication
+@EnableSwagger2
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+
+}
---
+++ books/src/main/java/pl/eud/pwr/ztw/books/IBooksService.java
@@ -0,0 +1,7 @@
+package pl.eud.pwr.ztw.books;
+
+import java.util.Collection;
+public interface IBooksService {
+    public abstract Collection<Book> getBooks();
+    public abstract Book getBook(int id);
+}
