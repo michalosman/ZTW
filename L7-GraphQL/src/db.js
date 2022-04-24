6f848cb860b79c701c0637007086aceadfa86a82
michalosman <michal.osman.997@gmail.com>
2022-04-25 00:29:40 +0200

    Finish L7

--- L7-GraphQL/src/db.js
+++ L7-GraphQL/src/db.js
@@ -1,31 +1,37 @@
-const users = [
-  {
-    id: 1,
-    name: "Jan Konieczny",
-    email: "jan.konieczny@wonet.pl",
-    login: "jkonieczny",
-  },
-  {
-    id: 2,
-    name: "Anna Wesołowska",
-    email: "anna.w@sad.gov.pl",
-    login: "anna.wesolowska",
-  },
-  {
-    id: 3,
-    name: "Piotr Waleczny",
-    email: "piotr.waleczny@gp.pl",
-    login: "p.waleczny",
-  },
-];
+import axios from "axios";
 
-const todos = [
-  { id: 1, title: "Naprawić samochód", completed: false, user_id: 3 },
-  { id: 2, title: "Posprzątać garaż", completed: true, user_id: 3 },
-  { id: 3, title: "Napisać e-mail", completed: false, user_id: 3 },
-  { id: 4, title: "Odebrać buty", completed: false, user_id: 2 },
-  { id: 5, title: "Wysłać paczkę", completed: true, user_id: 2 },
-  { id: 6, title: "Zamówic kuriera", completed: false, user_id: 3 },
-];
+const users = await fetchUsers();
+const todos = await fetchTodos();
+// const todos = [
+//   { id: 1, title: "Naprawić samochód", completed: false, user_id: 3 },
+//   { id: 2, title: "Posprzątać garaż", completed: true, user_id: 3 },
+//   { id: 3, title: "Napisać e-mail", completed: false, user_id: 3 },
+//   { id: 4, title: "Odebrać buty", completed: false, user_id: 2 },
+//   { id: 5, title: "Wysłać paczkę", completed: true, user_id: 2 },
+//   { id: 6, title: "Zamówic kuriera", completed: false, user_id: 3 },
+// ];
+
+async function fetchUsers() {
+  try {
+    const users = await axios.get("https://jsonplaceholder.typicode.com/users");
+    return users.data.map(({ id, name, email, username }) => ({
+      id: id,
+      name: name,
+      email: email,
+      login: username,
+    }));
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function fetchTodos() {
+  try {
+    const todos = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    return todos.data;
+  } catch (error) {
+    throw error;
+  }
+}
 
 export default { users, todos };
--- L7-GraphQL/src/resolvers.js
+++ L7-GraphQL/src/resolvers.js
@@ -4,20 +4,20 @@ const Query = {
   users: () => db.users,
   todos: () => db.todos,
   todo: (parent, args, context, info) =>
-    db.todos.find((todo) => todo.id == args.id),
+    db.todos.find((todo) => todo.id === Number.parseInt(args.id)),
   user: (parent, args, context, info) =>
-    db.users.find((user) => user.id == args.id),
+    db.users.find((user) => user.id === Number.parseInt(args.id)),
 };
 
 const User = {
   todos: (parent, args, context, info) => {
-    return db.todos.filter((todo) => todo.user_id === parent.id);
+    return db.todos.filter((todo) => todo.userId === parent.id);
   },
 };
 
 const TodoItem = {
   user: (parent, args, context, info) => {
-    return db.users.find((user) => user.id === parent.user_id);
+    return db.users.find((user) => user.id === parent.userId);
   },
 };
 
